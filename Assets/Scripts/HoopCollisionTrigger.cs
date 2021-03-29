f0a42b7250965a3c2e2e0a2ddaf26b37ff8d74d7
naomiwholt <54776733+naomiwholt@users.noreply.github.com>
2021-03-29 18:14:32 +1100

    Added logic to the hoops :)
    hoops now have colliders and shrink and disapear if you shoot through them

---
+++ Assets/Scripts/HoopCollisionTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopCollisionTrigger : MonoBehaviour
+{
+    public HoopDisapear HD;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HD = GetComponentInParent<HoopDisapear>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HD.disapear = true;
+    }
+}
---
+++ Assets/Scripts/HoopDisapear.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopDisapear : MonoBehaviour
+{
+    public bool disapear;
+    public SplodyTriggers XplosionTrigger;
+    public float ShrinkSpeed;
+    public AnimationCurve curve;
+    float Shrink;
+    float evaluate = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XplosionTrigger = GetComponentInChildren<SplodyTriggers>();
+    }
+    private void FixedUpdate()
+    {
+        if (disapear)
+        {           
+            evaluate += 0.1f;
+            evaluate = Mathf.Clamp(evaluate, 0f, 1f);
+            Shrink = curve.Evaluate(evaluate);
+            ScaleDown();
+            transform.eulerAngles += new Vector3(10,0,0);
+            if (transform.localScale.x < 0.5) { StartCoroutine(XplosionTrigger.PartyExplosion()); }
+        }
+    }
+    // Update is called once per frame
+
+  
+
+    void ScaleDown()
+    {
+        transform.localScale = transform.localScale - new Vector3(Shrink,0,Shrink); 
+    }
+}
