3dd52606c2bc3ae2b18c37f5f2594dff53dd473f
naomiwholt <54776733+naomiwholt@users.noreply.github.com>
2021-03-31 14:05:19 +1100

    Added new sky and changed colours :)

---
+++ Assets/Scripts/HoopBarrier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopBarrier : MonoBehaviour
+{
+    public bool triggered;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggered = true;
+        other.gameObject.GetComponent<HoopMovement>().HoopVelocity = other.gameObject.GetComponent<HoopMovement>().HoopVelocity *- 1;
+    }
+}
--- Assets/Scripts/HoopMovement.cs
+++ Assets/Scripts/HoopMovement.cs
@@ -6,15 +6,38 @@ public class HoopMovement : MonoBehaviour
 {
     public GameObject PlayerPlatform;
     public float HoopVelocity;
-    // Start is called before the first frame update
-    void Start()
+    public bool DirectionChange = false;
+    public float Range;
+    public float changetimer;
+    public bool changedFirstTime = false;
+    // Update is called once per frame
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         transform.RotateAround(PlayerPlatform.transform.position, Vector3.up, HoopVelocity * Time.deltaTime);
+
+        if (DirectionChange == false) { changetimer += 1 *Time.deltaTime; }
+        
+        if (changetimer > Range || changedFirstTime == false && changetimer*2 > Range)
+        {
+            changedFirstTime = true;
+            DirectionChange = true;
+            ChangeDirection();
+            changetimer = 0;
+        }
+        else
+        {
+            DirectionChange = false;
+        }
+        
+   
+    }
+
+    void ChangeDirection()
+    {
+        HoopVelocity *= -1;
     }
 }
