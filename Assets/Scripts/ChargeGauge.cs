719f654a2f6598fbeed735f9d6cbc067c5e312d7
VillyL <villyliaodev@gmail.com>
2021-04-26 22:44:08 +1000

    UI tweaks and added updated UI gauges

--- Assets/Scripts/ChargeGauge.cs
+++ Assets/Scripts/ChargeGauge.cs
@@ -6,20 +6,23 @@ using UnityEngine.UI;
 
 public class ChargeGauge : MonoBehaviour
 {
-    private Canvas ownCanvas;
-    private Slider loadGauge;
+    //private Canvas ownCanvas;
+    //private Slider loadGauge;
     public GameObject HandAnchor;
     public bool IsRightController;
     //public GameObject Controller;
     private InteractionController refValue;
 
+    public GameObject[] gaugeSections;
+    private float middleThreshold;
+    private float upperThreshold;
     //some inefficient stuff to put in functionality for now
     //private float gaugeMax;
     // Start is called before the first frame update
     void Start()
     {
-        ownCanvas = GetComponent<Canvas>();
-        loadGauge = GetComponentInChildren<Slider>();
+        //ownCanvas = GetComponent<Canvas>();
+        //loadGauge = GetComponentInChildren<Slider>();
         //GaugeStart();
         //Findnt<RightButtonController>().EnableGauge += GaugeStart;
         if (IsRightController == true)
@@ -33,26 +36,44 @@ public class ChargeGauge : MonoBehaviour
             FindObjectOfType<LeftButtonController>().DisableGauge += GaugeStop;
         }
         refValue = HandAnchor.GetComponent<InteractionController>();
-        loadGauge.maxValue = refValue.MaxReleaseForce;
+        //loadGauge.maxValue = refValue.MaxReleaseForce;
+        middleThreshold = refValue.MaxReleaseForce * 2/3;
+        upperThreshold = refValue.MaxReleaseForce;
     }
 
     // Update is called once per frame
     void FixedUpdate()  //needs optimising later
     {
-        if (ownCanvas.enabled == true)
+        if (gaugeSections[0].GetComponent<MeshRenderer>().enabled == true)
         {
-            loadGauge.value = refValue.releaseforce;
-            Debug.Log("Hi");
+            //loadGauge.value = refValue.releaseforce;
+            //Debug.Log("Hi");
+            if (refValue.releaseforce >= middleThreshold)
+            {
+                gaugeSections[1].GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            if (refValue.releaseforce == upperThreshold)
+            {
+                gaugeSections[2].GetComponent<MeshRenderer>().enabled = true;
+            }
         }
+
+        
     }
 
     public void GaugeStart()
     {
-        ownCanvas.enabled = true;
+        //ownCanvas.enabled = true;
+        gaugeSections[0].GetComponent<MeshRenderer>().enabled = true;
     }
 
     public void GaugeStop()
     {
-        ownCanvas.enabled = false;
+        //ownCanvas.enabled = false;
+        foreach (GameObject obj in gaugeSections)
+        {
+            obj.GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 }
